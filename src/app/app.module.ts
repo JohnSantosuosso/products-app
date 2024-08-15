d943755b2c9048a43bea804f123e7e35676d4554
John Santosuosso <john.santosuosso@omf.com>
2024-08-15 16:21:05 -0400

    generate copyright directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { ProductsModule } from './products/products.module';
 import { AppComponent } from './app.component';
+import { CopyrightDirective } from './copyright.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CopyrightDirective,
   ],
   imports: [
     BrowserModule,
