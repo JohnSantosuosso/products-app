0da02383aaf08c64762ef0a734c0e5ec37d4b039
John Santosuosso <john.santosuosso@omf.com>
2024-08-21 15:01:21 -0400

    add directive that has no use except to see how directives with hostbinding and listening work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { ProductsModule } from './products/products.module';
 import { AppComponent } from './app.component';
 import { CopyrightDirective } from './copyright.directive';
+import { NumericDirective } from './numeric.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CopyrightDirective,
+    NumericDirective,
   ],
   imports: [
     BrowserModule,
